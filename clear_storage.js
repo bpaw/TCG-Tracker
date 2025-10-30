bd9ab12921540ba6af06856bfe2cf0e67f364a6a
bpaw <bpaw.dev@gmail.com>
2025-10-29 22:32:29 -0700

    cleaning up files

--- clear_storage.js
+++
@@ -1,6 +0,0 @@
-// To clear storage programmatically, add this to App.tsx temporarily:
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// Add before initialize():
-await AsyncStorage.clear();
-console.log('Storage cleared!');
